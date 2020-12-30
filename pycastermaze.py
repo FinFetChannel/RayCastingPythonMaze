9fa44ec375d9d2a9f28529868eda63deaec02cc1
FinFET <76776190+FinFetChannel@users.noreply.github.com>
2020-12-30 10:46:22 -0300

    Added a few comments

--- pycastermaze.py
+++ pycastermaze.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import keyboard
 
+#random map generator
 size = 15
 mapa = [[list(np.random.uniform(0, 1, 3))] * size for i in range(size)]
 for i in range(size-2):
@@ -11,11 +12,9 @@ for i in range(size-2):
 
 posx, posy = (1, np.random.randint(1, size -1))
 rot = np.pi/4
-
 x, y = (posx, posy)
 mapa[x][y] = 0
-
-count = 0
+count = 0 
 while True:
     testx, testy = (x, y)
     if np.random.uniform() > 0.5:
@@ -33,18 +32,18 @@ while True:
         else:
             count = count+1
 
-while True:
+while True: #main game loop
     
     plt.hlines(-0.6, 0, 60, colors='gray', lw=165, alpha=0.5)
     plt.hlines(0.6, 0, 60, colors='lightblue', lw=165, alpha=0.5)
     tilex, tiley, tilec = ([], [], [])
     
-    for i in range(60):
+    for i in range(60): #vision loop
         rot_i = rot + np.deg2rad(i - 30)
         x, y = (posx, posy)
         sin, cos = (0.02*np.sin(rot_i), 0.02*np.cos(rot_i))
         n = 0
-        while True:
+        while True: # ray loop
             xx, yy = (x, y)
             x, y = (x + cos, y + sin)
             n = n+1
@@ -60,13 +59,14 @@ while True:
                 h = np.clip(1/(0.02 * n), 0, 1)
                 c = np.asarray(mapa[int(x)][int(y)])*(0.3 + 0.7 * h**2)
                 break 
-        plt.vlines(i, -h, h, lw = 8, colors = c)
+        plt.vlines(i, -h, h, lw = 8, colors = c) # draw vertical lines
 
-    plt.scatter(tilex, tiley, c=tilec)
+    plt.scatter(tilex, tiley, c=tilec) # draw tiles on the floor
     
     plt.axis('off'); plt.tight_layout(); plt.axis([0, 60, -1, 1])
     plt.draw(); plt.pause(0.0001); plt.clf()
 
+    # player's movement
     key = keyboard.read_key()
     x, y = (posx, posy)
 
