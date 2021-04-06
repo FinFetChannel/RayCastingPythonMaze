2371244013d1931f859b105b0a9d369717f37cfa
FinFET <76776190+FinFetChannel@users.noreply.github.com>
2021-04-06 00:19:27 -0300

    Update pycasting2.0.py

--- pycasting2.0.py
+++ pycasting2.0.py
@@ -1,15 +1,20 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from pynput import keyboard, mouse
+from time import time
 
 def main():
     size = 15
     global key; key = None # register keypresses
     listener = keyboard.Listener(on_press=on_press);listener.start()
-    posx, posy, rot, rot_v = (1, np.random.randint(1, size -1), 1, 0) # player pos
+    last_mouse = [0,0]
+    posx, posy, rot = (1, np.random.randint(1, size -1), 1) # player pos
     bg = np.linspace(0, 1, 150) #background gradient
     mapc, maph, mapr, ex, ey = maze_generator(posx, posy, size)# map, exit
+    plt.figure(num = 'Pycaster 2.0')
     while True: #main game loop
+        start = time()
+        rot, last_mouse = rotation(rot, last_mouse)
         plt.hlines(-0.5, 0, 60, colors='k', lw=165, alpha=np.sin((rot+np.pi/2)/2)**2/2)
         plt.hlines(0.5, 0, 60, colors='k', lw=165, alpha=np.sin((rot-np.pi/2)/2)**2/2)
         plt.scatter([30]*150, -bg, c=-bg, s=200000, marker='_', cmap='Greys')
@@ -18,28 +23,26 @@ def main():
         for i in range(60): #vision loop
             rot_i = rot + np.deg2rad(i - 30)
             x, y = (posx, posy)
-            sin, cos = (0.02*np.sin(rot_i), 0.02*np.cos(rot_i))
+            sin, cos = (0.04*np.sin(rot_i), 0.04*np.cos(rot_i))
             n, half = 0, None
             c, h, x, y, n, half, tx, ty, tc = caster(x, y, i, ex, ey, maph, mapc, sin, cos, n, half, tx, ty, tc)
             
             if mapr[int(x)][int(y)] == 1:
                 c, h, x, y, n, half2, tx, ty, tc = reflection(x, y, i, ex, ey, maph, mapc, sin, cos, n, c, h, half, tx, ty, tc)
-                if mapr[int(x)][int(y)] != 0:# and half ==  None:
-                    c, h, x, y, n, half2, tx, ty, tc = reflection(x, y, i, ex, ey, maph, mapc, sin, cos, n, c, h, half2, tx, ty, tc)
 
             else:
-                plt.vlines(i, -h, h, lw = 8, colors = c) # draw vertical lines
+                plt.vlines(i, -h, h, lw = 8, colors = c)
                 if half !=  None:
                     plt.vlines(i, -half[0], 0, lw = 8, colors = half[1])
+            
 
-        plt.scatter(tx, ty, c=tc, s=-50*np.asarray(ty), zorder = 2, alpha=0.3, marker='s') # draw ts on the floor
-        
+            
         plt.axis('off'); plt.tight_layout(); plt.axis([0, 60, -1, 1])
-        plt.draw(); plt.pause(0.0001); plt.clf()
-
+        plt.scatter(tx, ty, c=tc, zorder = 2, alpha=0.5, marker='s') # draw ts on the floor
+        plt.text(57, 0.9, str(round(1/(time()-start),1)), c='y')
+        plt.draw();plt.pause(0.1); plt.clf()
         # player's movement
-        rot, rot_v = rotation(rot, rot_v)
-        posx, posy, rot, rot_v, keyout = movement(posx, posy, rot, rot_v, maph)
+        posx, posy, rot, keyout = movement(posx, posy, rot, maph)
         if (int(posx) == ex and int(posy) == ey) or keyout == 'esc':
             break
 
@@ -71,20 +74,26 @@ def maze_generator(x, y, size):
                 count = count+1
     return np.asarray(mapc), np.asarray(maph), np.asarray(mapr), ex, ey
 
-def rotation(rot, rot_v): # for 1080p screen, 400px deadzone
+def rotation(rot, last_mouse): # for 1080p screen
     with mouse.Controller() as check:
         position = check.position
-        if (960-position[0])**2+ (540 - position[1])**2 > 40000:
-            rot = np.real(rot - (960 - position[0])/4800)
-            rot_v = np.real(rot_v + (540 - position[1])/2700)
+        if position[0] != last_mouse[0] or position[0]>1860 or position[0] < 60:
+            delta = last_mouse[0] - position[0]
+            if position[0]>1860:
+                delta = 1860 - position[0]
+            if position[0] < 60:
+                delta = 60 - position[0]
 
-    return(rot, rot_v)
+            rot = rot + 4*np.pi*(0.5-delta/1920)
+
+
+    return(rot, position)
 
 def on_press(key_new):
     global key
     key = key_new
     
-def movement(posx, posy, rot, rot_v, maph):
+def movement(posx, posy, rot, maph):
     global key
     x, y = (posx, posy)
     keyout = None
@@ -97,17 +106,13 @@ def movement(posx, posy, rot, rot_v, maph):
             rot = rot - np.pi/8
         elif key == keyboard.Key.right:
             rot = rot + np.pi/8
-        elif key == keyboard.Key.end:
-            rot_v = rot_v - np.pi/16
-        elif key == keyboard.Key.home:
-            rot_v = rot_v + np.pi/16
         elif key == keyboard.Key.esc:
             keyout = 'esc'
     key = None        
     if maph[int(x)][int(y)] == 0:
         posx, posy = (x, y)
         
-    return posx, posy, rot, rot_v, keyout
+    return posx, posy, rot, keyout
 
 def caster(x, y, i, ex, ey, maph, mapc, sin, cos, n, half, tx, ty, tc):
     while True: # ray loop
@@ -116,24 +121,54 @@ def caster(x, y, i, ex, ey, maph, mapc, sin, cos, n, half, tx, ty, tc):
         n = n+1
         if half == None and int(x*2)%2 == int(y*2)%2:#(abs(int(3*xx)-int(3*x)) > 0 or abs(int(3*yy)-int(3*y))>0):
             tx.append(i)
-            ty.append(-1/(0.02 * n*np.cos(np.deg2rad(i - 30))))
+            ty.append(-1/(0.04 * n*np.cos(np.deg2rad(i - 30))))
             if int(x) == ex and int(y) == ey:
                 tc.append('b')
             else:
                 tc.append('k')
-                
-        if maph[int(x)][int(y)] == 1:
+        if maph[int(x)][int(y)] == 1 or (maph[int(x)][int(y)] == 0.5 and half == None):
             h , c = shader(n, maph, mapc, sin, cos, x, y, i)
-            break
-        if maph[int(x)][int(y)] == 0.5 and half == None:
-            h , c = shader(n, maph, mapc, sin, cos, x, y, i)
-            half = [h, c]
+            if maph[int(x)][int(y)] == 0.5 and half == None:
+                half = [h, c, n]
+            else:
+                break
 
     return(c, h, x, y, n, half, tx, ty, tc)
 
+def caster3(x, y, i, ex, ey, maph, mapc, sin, cos, n, half, tx, ty, tc):
+    x, y, h2, n, tx2, ty2, tc2 = ray(x, y, i, ex, ey, maph, sin, cos, half, n)
+    tx, ty, tc = tx+tx2, ty+ty2, tc+tc2
+    h , c = shader(n, maph, mapc, sin, cos, x, y, i)
+    print(h2)
+    if half == None and h2 != None:
+        hh , cc = shader(h2[0], maph, mapc, sin, cos, h2[1], h2[2], i)
+        half = [hh, cc]
+        
+    return(c, h, x, y, n, half, tx, ty, tc)
+
+#@njit#(fastmath=True)
+def ray(x, y, i, ex, ey, maph, sin, cos, half, n):
+    tx, ty, tc  = list([]), list([]), list([])
+    while True: # ray loop
+        xx, yy = (x, y)
+        x, y = (x + cos, y + sin)
+        n = n+1
+        if half == None:
+            if int(x*2)%2 == int(y*2)%2:#(abs(int(3*xx)-int(3*x)) > 0 or abs(int(3*yy)-int(3*y))>0):
+                tx.append(i)
+                ty.append(-1/(0.04 * n*np.cos(np.deg2rad(i - 30))))
+                if int(x) == ex and int(y) == ey:
+                    tc.append('b')
+                else:
+                    tc.append('k')
+            if maph[int(x)][int(y)] == 0.5:
+                half = [n, x, y]
+        if maph[int(x)][int(y)] == 1:
+            break
+    return x, y, half, n, tx, ty, tc
 
 def shader(n, maph, mapc, sin, cos, x, y, i):
-    h = np.clip(1/(0.02 * n*np.cos(np.deg2rad(i-30))), 0, 1)
+    h = np.clip(1/(0.04 * n*np.cos(np.deg2rad(i-30))), 0, 1)
     c = np.asarray(mapc[int(x)][int(y)])*(0.4 + 0.6 * h)
     if maph[int(x+cos)][int(y-sin)] != 0:
         c = 0.85*c
